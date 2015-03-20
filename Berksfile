c371f8e980d426e2f8a08d9a804cf47b03bfa07c
Charles Johnson <charles@opscode.com>
2015-03-20 14:01:26 -0700

    Added Kitchen tests
    Added some basic kitchen tests

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
+cookbook 'apt'
+
 metadata
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,12 +3,27 @@
 require 'spec_helper'
 
 describe 'testcookbook::default' do
-
+  #
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
 
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  # Serverspec cannot (and should not!) read Chef attributes, so
+  # any per-os conditionals have to be re-defined in the test.
+  if %w(debian ubuntu).include?(os[:family])
+    service_name = 'apache2'
+  else
+    service_name = 'httpd'
   end
 
+  it 'should configure the server with a started webserver' do
+    expect(service(service_name)).to be_running
+  end
+
+  it 'should configure the server with an enabled webserver' do
+    expect(service(service_name)).to be_enabled
+  end
+
+  it 'should configure the server to listen on TCP port 80' do
+    expect(port(80)).to be_listening
+  end
 end
