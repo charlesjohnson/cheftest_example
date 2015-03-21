368f1180dd1e4499c15fa958edaca18798f58dd0
Charles Johnson <charles@opscode.com>
2015-03-21 15:45:08 -0700

    Updated documentation

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name             'testcookbook'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures testcookbook'
-long_description 'Installs/Configures testcookbook'
+maintainer       'Charles Johnson'
+maintainer_email 'charles@chef.io'
+license          'Apache 2.0'
+description      'A simple cookbook to teach Chef testing'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'redhat'
+supports 'ubuntu'
